d384f490993021befe0a2045f0d3df6720d374af
unknown <imamovica1397@gmail.com>
2021-03-27 09:21:19 +0100

    Created

---
+++ database/migrations/2021_03_21_221507_create_countries_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Database\Seeders\CountriesTableSeeder;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->string('image')->default('none');
+            $table->timestamps();
+        });
+
+        Artisan::call('db:seed', [
+            '--class' => CountriesTableSeeder::class
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
